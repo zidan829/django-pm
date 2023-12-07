6022836bd537901fdb636ce1d988c793bd258705
zidan829 <130111055+zidan829@users.noreply.github.com>
2023-12-08 02:05:36 +0300

    Registration & profile update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from typing import Any
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm,UserChangeForm
 from django import forms
-
+from django.contrib.auth.models import User
 attrs={'class':'form-control'}
 class UserLoginForm(AuthenticationForm):
     def __init__(self,*args,**kwargs):
@@ -13,6 +13,51 @@ username=forms.CharField(
 password=forms.CharField(
     label='Password',
     widget=forms.PasswordInput(attrs=attrs)
-    
+
 )
+
+class UserRegisterForm(UserCreationForm):
+
+
+    first_name=forms.CharField(
+    label='First Name',
+    widget=forms.TextInput(attrs=attrs)
+    )
+    last_name=forms.CharField(
+    label='Last Name',
+    widget=forms.TextInput(attrs=attrs)
+)
+
+    username=forms.CharField(
+    label='Username',
+    widget=forms.TextInput(attrs=attrs)
+)
+    email=forms.EmailField(
+    label='Email',
+    widget=forms.TextInput(attrs=attrs)
+)
+    password1=forms.CharField(
+    label='Password',
+    strip=False,
+    widget=forms.PasswordInput(attrs=attrs)
+)
+    password2=forms.CharField(
+    label='Password confirmation',
+    strip=False,
+    widget=forms.PasswordInput(attrs=attrs)
+)
+
+    class Meta(UserCreationForm.Meta):
+        fields=('first_name','last_name','username','email')
     
+
+class ProfileForm(UserChangeForm):
+    password=None
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email']
+        widgets={
+            'first_name':forms.TextInput(attrs=attrs),
+            'last_name':forms.TextInput(attrs=attrs),
+            'email':forms.EmailInput(attrs=attrs),
+        }
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from django.contrib.auth.views import LoginView
 from accounts.forms import UserLoginForm
+from accounts.views import RegisterView,edit_profile
 urlpatterns=[
     path('',include('django.contrib.auth.urls')),
-    path('login/',LoginView.as_view(authentication_form=UserLoginForm),name='login')
+    path('login/',LoginView.as_view(authentication_form=UserLoginForm),name='login'),
+    path('register/',RegisterView.as_view(),name='register'),
+    path('profile/',edit_profile,name='profile')
 ]
